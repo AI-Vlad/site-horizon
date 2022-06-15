09023b2e413afdbc28580d333e0d4d0eddd39a11
Vlad-X <vladrojas@hotmail.com>
2022-06-14 20:10:41 -0400

    adicionando validação ao recaptcha

--- assets/js/validateFormMail.js
+++ assets/js/validateFormMail.js
@@ -2,15 +2,12 @@ class ValidateFormMail {
     constructor() {
         this.sumbitButton = document.querySelector('.btn')
 
-        this.validate()
+
     }
 
     validate() {
-        this.sumbitButton.addEventListener('sumbit', e => {
-            e.preventDefault()
-            return grecaptcha.getResponse() ? alert('deu certo') : alert('marque a caixa!')
-        })
+        return grecaptcha.getResponse() ? alert('deu certo') : alert('marque a caixa!')
     }
 }
 
-window.App = new ValidateFormMail()
+let validateFormMail = new ValidateFormMail()
